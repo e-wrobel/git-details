5693ee84a62b51b07dd0b18f81f92e59a6d12e1b
Marcin Wroblewski <marcin.wroblewski@aol.com>
2020-08-28 14:27:26 +0200

    Added arg parser

--- README.md
+++ README.md
@@ -2,5 +2,12 @@
 This repository contains tool for getting git details - mainly details about commits - for selected user, and since given date.
 It can be used to show your work progress on selected repository (and also branch).
 
+# Remark:
+You need to install git command line tool.
+
 # How to use it:
-An example is showed in runner.py. Remember that this tool relies on git command installed in the system.
+Example usage: 
+python3 git-details.py /home/ubuntu/my_repo "Mr Sparrow" 2020-08-01 --details Y
+
+OR when running from installed package in native/docker/virtualnenv: 
+git-details /home/ubuntu/my_repo "Mr Sparrow" 2020-08-01 --details Y
---
+++ git_details/git-details.py
@@ -0,0 +1,32 @@
+from git_details.gitlib import GitDetails
+import argparse
+
+example_usage = 'Example usage: \npython3 git-details.py /home/ubuntu/my_repo "Mr Sparrow" 2020-08-01 --details Y ' \
+                '\nOR when running from installed package in native/docker/virtualnenv: ' \
+                '\ngit-details /home/ubuntu/my_repo "Mr Sparrow" 2020-08-01 --details Y'
+
+
+def parse():
+    parser = argparse.ArgumentParser(description='Git details for selected user and repository',
+                                     epilog=example_usage)
+    parser.add_argument('path', type=str,
+                        help='path to local repository')
+    parser.add_argument('user', type=str,
+                        help='Search will be narrowed to the selected user, for example \"Mr Sparrow\"')
+    parser.add_argument('since', type=str,
+                        help='Search will be narrowed to selected since date, for example: 2020-08-01')
+    parser.add_argument('--details', type=str, default='Y',
+                        help='To print just commits or whole changes (git show): Y/N')
+    arguments = parser.parse_args()
+
+    return arguments
+
+
+if __name__ == '__main__':
+    args = parse()
+
+    details = args.details
+    details = True if details == 'Y' else False
+
+    g = GitDetails(repo_path=args.path, user_name=args.user, since_date=args.since)
+    g.get_commits(print_details=details)
